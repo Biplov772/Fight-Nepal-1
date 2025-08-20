ed1c427deb90e9c8eae28397cae822fe9ede512c
Ashok2025-eng <gautamashok7723@gmail.com>
2025-08-20 10:47:49 +0545

    feat:impl database connection & create class

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 from fastapi import FastAPI
 import sqlite3
-app = FastAPI()
+from pydantic import BaseModel
+
 con = sqlite3.connect("fights.db")
- 
-@app.get("/")
+
+def initalize_databse():
+    create_event_table = """
+    CREATE TABLE events (
+        eventName TEXT,
+        participantOne TEXT,
+        participantTwo TEXT,
+        time TEXT,
+        winner TEXT
+    );
+    """
+
+app = FastAPI()
+
+
+@app.get("/")   
 def main():
-    return"Hello from fight-nepal!"
+    return "flight-nepal"
+
+
+class CreateEventRequest(BaseModel):
+    eventName: str
+    participantOne: str
+    participantTwo: str
+    time: str
+    winner: str
+
 
 @app.post("/event")
-def create_event():
-    return "creating an event"
+def create_event(event: CreateEventRequest):
+    return event
+
+
 @app.get("/event/delete")
 def delete_event():
-    return "deleting and event with id"
+    return "delete event"
+
+
 @app.get("/event/get-all")
 def get_all_events():
-    return "all events"
+    return "All event"
+
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
